d26f190437a5b90a2550f24fd4a721760c712ed8
Sergio Rodrigo <sistemas.shema.OP1@gmail.com>
2025-09-16 13:24:02 -0400

    login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 'use client'
 import { Button } from "@heroui/react";
+// import { useCallback, useEffect, useState } from "react";
 // import Image from "next/image";
 
 export default function Home() {
+  // const [counter, setCounter] = useState(5)
+
+  // const decrementCount = useCallback(()=>{
+  //   setCounter(prevCounter => prevCounter - 1)
+  // }, [])
+
+
+  // useEffect(()=>{
+
+  //   if(counter==0){
+  //     console.log("llego a cero")
+  //     return 
+  //   }
+
+  //   setInterval(decrementCount, 1000)
+  // }, [])
   return (
     <div 
     // className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
@@ -10,7 +27,7 @@ export default function Home() {
       <main 
       // className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"
       >
-
+        {/* {counter} */}
         <Button color="primary">This is a button</Button>
       </main>
         
