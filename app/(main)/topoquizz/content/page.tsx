3746cafcb10f69928df56f6b1ebced869bfa9b3a
Sergio Rodrigo <sistemas.shema.OP1@gmail.com>
2025-10-29 14:10:50 -0400

    temporal commit

--- app/(main)/topoquizz/content/page.tsx
+++ app/(main)/topoquizz/content/page.tsx
@@ -15,7 +15,52 @@ import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headless
 import clsx from "clsx";
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 
-import { ICoursesData } from "@/interfaces/topoquizz";
+import { ICoursesData, ILessonData } from "@/interfaces/topoquizz";
+
+// interface ICustomListBox{
+
+// }
+
+// const CustomListox:React.FC<ICustomListBox> = ()=>{
+//   return(
+//     <Listbox
+//       value={selected} 
+//       onChange={setSelected}
+//     >
+//       <ListboxButton
+//         className={clsx(
+//           'relative block w-full rounded-lg py-1.5 pr-8 pl-3 text-left text-sm/6 bg-black text-white',
+//           'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+//         )}
+//       >
+//         {selected?.name}
+//         <ChevronDownIcon
+//           className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white"
+//           aria-hidden="true"
+//         />
+//       </ListboxButton>
+//       <ListboxOptions
+//         anchor="bottom"
+//         transition
+//         className={clsx(
+//           'w-(--button-width) rounded-xl border border-white/5 bg-black p-1 [--anchor-gap:--spacing(1)] focus:outline-none',
+//           'transition duration-100 ease-in data-leave:data-closed:opacity-0'
+//         )}
+//       >
+//         {dataFromServer.map((element, index) => (
+//           <ListboxOption
+//             key={index}
+//             value={element}
+//             className="group flex cursor-pointer items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-white/10"
+//           >
+//             <CheckIcon className="invisible size-4 fill-white group-data-selected:visible" />
+//             <div className="text-sm/6 text-white">{element.name}</div>
+//           </ListboxOption>
+//         ))}
+//       </ListboxOptions>
+//     </Listbox>)
+// }
+
 
 interface ContentPageProps {
 
@@ -24,12 +69,18 @@ interface ContentPageProps {
 interface ISubjectListProps {
   // selectedSubject: Set<string>,
   // methodSetSelectedSubject: Dispatch<SetStateAction<Set<string>>>;
+  // Correcto
   selectedSubject: ICoursesData | undefined,
-  methodSetSelectedSubject: Dispatch<SetStateAction<ICoursesData | undefined>>;
+  methodSetSelectedSubject: Dispatch<SetStateAction<ICoursesData>>;
 
 }
 
-const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetSelectedSubject }) => {
+const SubjectsList: React.FC<ISubjectListProps> = (
+  { 
+    selectedSubject, 
+    methodSetSelectedSubject 
+  }
+) => {
 
   const {
     coursesData: coursesDataFromFirebase,
@@ -37,22 +88,22 @@ const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetS
     error: errorGettingCoursesData
   } = getCourses()
 
-  const [selected, setSelected] = useState<ICoursesData>({
-    id:'',
-    enable:false, 
-    image:'',
-    name:'',
-    slug:'',
-    createdAt: null,
-    updatedAt: null
-  })
+  // const [selected, setSelected] = useState<ICoursesData>({
+  //   id:'',
+  //   enable:false, 
+  //   image:'',
+  //   name:'',
+  //   slug:'',
+  //   createdAt: null,
+  //   updatedAt: null
+  // })
   const [dataFromServer, setDataFromServer] = useState<ICoursesData[]>([]);
 
   useEffect(() => {
     
     if (coursesDataFromFirebase.length > 0) {
       setDataFromServer(coursesDataFromFirebase)
-      setSelected(coursesDataFromFirebase[0])
+      methodSetSelectedSubject(coursesDataFromFirebase[0])
     }
   }, [coursesDataFromFirebase])
 
@@ -75,9 +126,9 @@ const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetS
         </Button>
         {
           loadingCourses ? <div>Cargando...</div> : 
-          <Listbox 
-            value={selected} 
-            onChange={setSelected}
+          <Listbox
+            value={selectedSubject} 
+            onChange={methodSetSelectedSubject}
           >
             <ListboxButton
               className={clsx(
@@ -85,7 +136,7 @@ const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetS
                 'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
               )}
             >
-              {selected?.name}
+              {selectedSubject?.name}
               <ChevronDownIcon
                 className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white"
                 aria-hidden="true"
@@ -111,7 +162,7 @@ const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetS
               ))}
             </ListboxOptions>
           </Listbox>
-}
+        }
         {/* <Listbox
           // disabledKeys={["edit", "delete"]}
           disallowEmptySelection
@@ -136,20 +187,26 @@ const SubjectsList: React.FC<ISubjectListProps> = ({ selectedSubject, methodSetS
 }
 
 interface ILessonsListType {
-  courseSelected: string
-  selectedLesson: Set<string>
-  methodSetLessonSelected: Dispatch<SetStateAction<Set<string>>>;
+  courseSelected: ICoursesData
+  selectedLesson: ILessonData | undefined //Set<string>
+  methodSetLessonSelected: Dispatch<SetStateAction<ILessonData | undefined>>
 }
 
-const LessonsList: React.FC<ILessonsListType> = ({ courseSelected, selectedLesson, methodSetLessonSelected }) => {
+const LessonsList: React.FC<ILessonsListType> = ({ 
+  courseSelected, 
+  selectedLesson, 
+  methodSetLessonSelected }) => {
+  
   const {
     data: lessonsData,
     loading: loadingLessonsData,
-    error } = getLessonsByCourse(courseSelected) //courseSelected:string
+    error 
+  } = getLessonsByCourse(courseSelected.id) //courseSelected:string
 
   useEffect(() => {
     if (lessonsData.length > 0) {
-      methodSetLessonSelected(new Set([lessonsData[0].slug || String(0)]));
+      console.log(lessonsData)
+      // methodSetLessonSelected(new Set([lessonsData[0].slug || String(0)]));
     }
   }, [lessonsData])
 
@@ -163,8 +220,47 @@ const LessonsList: React.FC<ILessonsListType> = ({ courseSelected, selectedLesso
           </Button>
         </div>
         
-        {loadingLessonsData ? "Cargando..." : <></>}
-        {error ? error : <></>}
+        {
+          loadingLessonsData && courseSelected===null ? "Cargando..." : 
+          <>
+            <Listbox
+              value={selectedLesson} 
+              onChange={methodSetLessonSelected}
+            >
+              <ListboxButton
+                className={clsx(
+                  'relative block w-full rounded-lg py-1.5 pr-8 pl-3 text-left text-sm/6 bg-black text-white',
+                  'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+                )}
+              >
+                {selectedLesson?.name}
+                <ChevronDownIcon
+                  className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white"
+                  aria-hidden="true"
+                />
+              </ListboxButton>
+              <ListboxOptions
+                anchor="bottom"
+                transition
+                className={clsx(
+                  'w-(--button-width) rounded-xl border border-white/5 bg-black p-1 [--anchor-gap:--spacing(1)] focus:outline-none',
+                  'transition duration-100 ease-in data-leave:data-closed:opacity-0'
+                )}
+              >
+                {lessonsData.map((element, index) => (
+                  <ListboxOption
+                    key={index}
+                    value={element}
+                    className="group flex cursor-pointer items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-white/10"
+                  >
+                    <CheckIcon className="invisible size-4 fill-white group-data-selected:visible" />
+                    <div className="text-sm/6 text-white">{element.name}</div>
+                  </ListboxOption>
+                ))}
+              </ListboxOptions>
+            </Listbox>
+          </>
+        }
 
         {/* <Listbox
           selectedKeys={selectedLesson}
@@ -195,8 +291,16 @@ const ContentPage: React.FC<ContentPageProps> = () => {
   // const [courseIdSelected, setCourseIdSelected] = useState(new Set([""]));
   // const [lessonIdSelected, setLessonIdSelected] = useState(new Set([""]));
 
-  const [courseSelected, setCourseSelected] = useState<ICoursesData>();
-  const [lessonIdSelected, setLessonIdSelected] = useState('');
+  const [courseSelected, setCourseSelected] = useState<ICoursesData>({
+    id:'',
+    enable:false, 
+    image:'',
+    name:'',
+    slug:'',
+    createdAt: null,
+    updatedAt: null
+  });
+  const [lessonSelected, setLessonSelected] = useState<ILessonData>();
 
   return (
     <div className="h-screen w-screen">
@@ -215,20 +319,24 @@ const ContentPage: React.FC<ContentPageProps> = () => {
               methodSetSelectedSubject={setCourseSelected}
             />
           </div>
-          {/* <div className="grow">
+          <div 
+            // className="grow"
+          >
+
             <LessonsList
-              courseSelected={Array.from(courseIdSelected)[0] || ''} //first element
-              selectedLesson={lessonIdSelected}
-              methodSetLessonSelected={setLessonIdSelected}
+              courseSelected={courseSelected} //first element
+              selectedLesson={lessonSelected}
+              methodSetLessonSelected={setLessonSelected}
             />
-          </div> */}
+
+          </div>
         </div>
-        <div className="col-span-4">
+        {/* <div className="col-span-4">
           <QuestionsComponent
             lessonIdSeelected={Array.from(lessonIdSelected)[0] || ''}
           // questionsData={[]}
           />
-        </div>
+        </div> */}
 
         {/* <QuestionsTable
         questionsData={selectedLessonId ? questions : []}
